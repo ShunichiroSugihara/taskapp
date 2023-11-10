5f7c1261217b8642fa194ce14cfc3b200ad1fd2a
S.Sugi <kindabassman@gmail.com>
2023-11-10 15:13:27 +0900

    task.swiftの場所を変更した

---
+++ taskapp/Task.swift
@@ -0,0 +1,23 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 杉原俊一郎 on 2023/11/08.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @Persisted(primaryKey: true) var id: ObjectId
+
+    // タイトル
+    @Persisted var title = ""
+
+    // 内容
+    @Persisted var contents = ""
+
+    // 日時
+    @Persisted var date = Date()
+
+}
